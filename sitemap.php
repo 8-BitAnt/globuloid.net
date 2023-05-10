14cdfc0333498a09f2f13d442ad74772c9657fd1
8-BitAnt <69584738+8-BitAnt@users.noreply.github.com>
2023-05-10 15:13:12 +0000

    sitemap

--- sitemap.php
+++ sitemap.php
@@ -1,27 +1,47 @@
-<?php 
-//sitemap.php
-$connect = mysqli_connect("localhost", "root", "", "testing");
+<?php
+header("Content-type: text/xml");
 
-$query = "SELECT page_url FROM page";
+// Connect database
+$db_host = 'localhost';
+$db_user = 'root';
+$db_password = 'mysql';
+$db_name = 'sitemap_demo';
+$connect = mysqli_connect($db_host, $db_user, $db_password, $db_name);
 
-$result = mysqli_query($connect, $query);
+// Base or Home URL
+$base_url = 'http://localhost/_youtube/dynamic_sitemap/';
+/*
+ * sitemap attributes
+lastmod: When did the content last modified
+priority: the priority of the URL, relative to your own website on a scale between 0.0 and 1.0.
+changefreq: how often the content on the URL is expected to change.
+Possible values are always, hourly, daily, weekly, monthly, yearly and never.
+ * */
+// define basic structure of XML
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+echo '<sitemapindex xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">';
 
-$base_url = "/../";
+echo '<sitemap>'. PHP_EOL;
+echo '<loc>'.$base_url.'</loc>'. PHP_EOL;
+echo '<changefreq>daily</changefreq>'. PHP_EOL;
+echo '</sitemap>'. PHP_EOL;
 
-header("Content-Type: application/xml; charset=utf-8");
+// fetch all pages
+$sql = 'SELECT * FROM pages';
 
-echo '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL; 
+$result = mysqli_query($connect, $sql);
 
-echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">' . PHP_EOL;
+if(!empty($result)){ // see if we have anything in the result
+    // fetch single page
+    while($page = mysqli_fetch_object($result)){
+        // define a single page xml
+        $url = $base_url;
+        $url .= '?page='. $page->slug;
+        echo '<sitemap>'. PHP_EOL;
+        echo '<loc>'.$url.'</loc>'. PHP_EOL;
+        echo '<changefreq>daily</changefreq>'. PHP_EOL;
+        echo '</sitemap>'. PHP_EOL;
 
-while($row = mysqli_fetch_array($result))
-{
- echo '<url>' . PHP_EOL;
- echo '<loc>'.$base_url. $row["page_url"] .'/</loc>' . PHP_EOL;
- echo '<changefreq>daily</changefreq>' . PHP_EOL;
- echo '</url>' . PHP_EOL;
+    }
 }
-
-echo '</urlset>' . PHP_EOL;
-
-?>
+echo '</sitemapindex>'. PHP_EOL;
